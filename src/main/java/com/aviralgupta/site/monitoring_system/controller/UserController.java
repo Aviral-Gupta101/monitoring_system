9869f542e8cc2f6f73c9184fd3fcd11c77f1fb58
aviral gupta <aviralg2002@gmail.com>
2025-04-20 22:20:16 +0530

    Feat: Added validation for serverAddress to be of type IPv4 or domain name]

--- src/main/java/com/aviralgupta/site/monitoring_system/controller/UserController.java
+++ src/main/java/com/aviralgupta/site/monitoring_system/controller/UserController.java
@@ -3,9 +3,11 @@ package com.aviralgupta.site.monitoring_system.controller;
 import com.aviralgupta.site.monitoring_system.dto.MonitorDto;
 import com.aviralgupta.site.monitoring_system.entity.Monitor;
 import com.aviralgupta.site.monitoring_system.service.controller.UserService;
+import com.aviralgupta.site.monitoring_system.util.RequestValidatorUtil;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
 import java.util.List;
 import java.util.Map;
@@ -29,7 +31,10 @@ public class UserController {
     }
 
     @PostMapping("/monitor/create")
-    public ResponseEntity<?> createMonitor(@Valid @RequestBody MonitorDto monitorDto){
+    public ResponseEntity<?> createMonitor(@Valid @RequestBody MonitorDto monitorDto, BindingResult result){
+
+        if(result.hasErrors())
+            return RequestValidatorUtil.getErrors(result);
 
         Monitor createdMonitor = userService.createMonitor(monitorDto);
         return ResponseEntity.ok(createdMonitor);
--- src/main/java/com/aviralgupta/site/monitoring_system/dto/MonitorDto.java
+++ src/main/java/com/aviralgupta/site/monitoring_system/dto/MonitorDto.java
@@ -1,11 +1,14 @@
 package com.aviralgupta.site.monitoring_system.dto;
 
 import com.aviralgupta.site.monitoring_system.util.enums.MonitorTypeEnum;
+import jakarta.validation.constraints.AssertTrue;
 import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import lombok.Builder;
 import lombok.Data;
+import org.apache.commons.validator.routines.DomainValidator;
+import org.apache.commons.validator.routines.InetAddressValidator;
 
 
 @Data
@@ -23,5 +26,15 @@ public class MonitorDto {
     private Integer ScheduleInterval;
 
     private MonitorTypeEnum type;
+
+    @AssertTrue(message = "serverAddress must be a valid IPv4 address or domain name")
+    public boolean isServerAddressValid() {
+        if (serverAddress == null || serverAddress.isBlank()) return true; // let @NotBlank handle this
+
+        InetAddressValidator ipValidator = InetAddressValidator.getInstance();
+        DomainValidator domainValidator = DomainValidator.getInstance(true); // allow localhost, etc.
+
+        return ipValidator.isValidInet4Address(serverAddress) || domainValidator.isValid(serverAddress);
+    }
 
 }
