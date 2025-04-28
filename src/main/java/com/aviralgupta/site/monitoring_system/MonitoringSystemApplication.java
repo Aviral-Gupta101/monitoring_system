fa89d93af9d3b4f44dfe9e08491fb1c36351c3bf
Aviral Gupta <aviralg2002@gmail.com>
2025-04-28 07:19:25 +0530

    Update MonitoringSystemApplication.java

--- src/main/java/com/aviralgupta/site/monitoring_system/MonitoringSystemApplication.java
+++ src/main/java/com/aviralgupta/site/monitoring_system/MonitoringSystemApplication.java
@@ -3,21 +3,11 @@ package com.aviralgupta.site.monitoring_system;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import java.util.concurrent.*;
-
 @SpringBootApplication
 public class MonitoringSystemApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(MonitoringSystemApplication.class, args);
-
-//        ScheduledExecutorService service = Executors.newScheduledThreadPool(10);
-//
-//        ScheduledFuture<?> future = service.scheduleWithFixedDelay(() -> {
-//            System.out.println("Hello world");
-//        }, 0, 2, TimeUnit.SECONDS);
-
-
 
     }
 
